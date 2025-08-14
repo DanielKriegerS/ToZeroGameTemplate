74fba9d0f034d92f5b05e126a482acb546cfe760
dschifel <danielschifelbain@gmail.com>
2025-08-14 17:32:35 -0300

    New: created navbar component

---
+++ src/component/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faInfo, faGear, faPlay } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss', 'navbar.media.component.scss']
+})
+export class NavbarComponent {
+
+  faInfo = faInfo;
+  faGear = faGear;
+  faPlay = faPlay;
+
+}
