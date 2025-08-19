f390b8726bcf248ba9e7248997a7df393116033e
dschifel <danielschifelbain@gmail.com>
2025-08-19 18:15:31 -0300

    Feat:  implemented route to main content component as default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', 
+        loadComponent: () => 
+            import('../component/main-content/main-content.component').then(
+                (m) => m.MainContentComponent
+            ) 
+    },
+    { path: '**', redirectTo: '' },
+];
