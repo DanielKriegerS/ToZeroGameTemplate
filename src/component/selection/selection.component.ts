c9a41f3421ea9bbbe4225319ecbe8a1269318f5d
dschifel <danielschifelbain@gmail.com>
2025-08-19 18:14:13 -0300

    New: created selection content component

---
+++ src/component/selection/selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selection',
+  imports: [],
+  templateUrl: './selection.component.html',
+  styleUrls: ['./selection.component.sass', '../../styles.scss']
+})
+export class SelectionComponent {
+
+}
