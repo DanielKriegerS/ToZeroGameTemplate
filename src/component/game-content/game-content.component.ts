d39597d2881d8c10634998f584ffc557c77d34e5
dschifel <danielschifelbain@gmail.com>
2025-08-22 19:14:25 -0300

    New: Implemented son's methods + mocked test info

--- src/component/game-content/game-content.component.ts
+++ src/component/game-content/game-content.component.ts
@@ -10,5 +10,47 @@ import { ButtonsComponent } from "../buttons/buttons.component";
   styleUrl: './game-content.component.sass'
 })
 export class GameContentComponent {
+  config = {
+    playerName: 'Daniel',
+    timer: 60
+  };
+
+
+  selectedNumber1: number | null = 5;
+  selectedNumber2: number | null = 3;
+  operation: string | null = '+';
+  result: number | null = 8;
+
+  onExecute() {
+    if (this.selectedNumber1 !== null && this.selectedNumber2 !== null && this.operation) {
+      switch (this.operation) {
+        case '+':
+          this.result = this.selectedNumber1 + this.selectedNumber2;
+          break;
+        case '-':
+          this.result = this.selectedNumber1 - this.selectedNumber2;
+          break;
+        case '*':
+          this.result = this.selectedNumber1 * this.selectedNumber2;
+          break;
+        case '/':
+          this.result = this.selectedNumber2 !== 0 ? this.selectedNumber1 / this.selectedNumber2 : null;
+          break;
+        case '^':
+          this.result = this.selectedNumber1 ** this.selectedNumber2;
+      }
+    }
+  }
+
+  onCancel() {
+    this.selectedNumber1 = null;
+    this.selectedNumber2 = null;
+    this.operation = null;
+    this.result = null;
+  }
+
+  onRestart() {
+    this.onCancel();
+  }
 
 }
