6c021d45c40f829cb3c50ce4e972047998f6ab39
dschifel <danielschifelbain@gmail.com>
2025-08-22 19:06:51 -0300

    New: created input config

--- src/component/config-feedback/config-feedback.component.ts
+++ src/component/config-feedback/config-feedback.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-config-feedback',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './config-feedback.component.html',
   styleUrl: './config-feedback.component.sass'
 })
 export class ConfigFeedbackComponent {
 
+  @Input() config!: {playerName: string, timer: number};
 }
