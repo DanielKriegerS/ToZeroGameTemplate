aa39e7b3743a15e0e26e46fa2d055396342d69d1
dschifel <danielschifelbain@gmail.com>
2025-08-22 19:06:27 -0300

    New: created output emitters + implemented icons

--- src/component/buttons/buttons.component.ts
+++ src/component/buttons/buttons.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowsRotate, faCheck, faEraser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-buttons',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './buttons.component.html',
-  styleUrl: './buttons.component.sass'
+  styleUrls: ['./buttons.component.sass']
 })
 export class ButtonsComponent {
+  @Output() execute = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+  @Output() restart = new EventEmitter<void>();
 
+  faCheck = faCheck;
+  faEraser = faEraser;
+  faArrowsRotate = faArrowsRotate;
 }
