555545924f650866927a28a551f63d84d1d9471d
dschifel <danielschifelbain@gmail.com>
2025-08-22 18:58:20 -0300

    Feat: Implemented navigation with links

--- src/component/navbar/navbar.component.ts
+++ src/component/navbar/navbar.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faInfo, faGear, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from '../../models/NavLinkt';
+import { RouterModule } from '@angular/router';
+import { NgClass, NgForOf } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule, NgForOf, NgClass],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss', 'navbar.media.component.scss', '../../styles.scss', '../../media-styles.scss']
 })
+
 export class NavbarComponent {
 
   faInfo = faInfo;
   faGear = faGear;
   faPlay = faPlay;
+
+  links: NavLink[] = [
+    { path: './index.html', label: 'jogar', icon: this.faPlay },
+    { path: './pages/info-page.html', label: 'como jogar', icon: this.faInfo },
+    { path: './pages/config-page.html', label: 'configurações', icon: this.faGear }
+  ];
 
 }
