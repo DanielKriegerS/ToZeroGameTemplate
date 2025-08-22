c5dadd39ced244d4c48fd5177abf26178e793729
dschifel <danielschifelbain@gmail.com>
2025-08-22 19:20:22 -0300

    New: created input values

--- src/component/selection/selection.component.ts
+++ src/component/selection/selection.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selection',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './selection.component.html',
   styleUrls: ['./selection.component.sass', '../../styles.scss']
 })
 export class SelectionComponent {
-
+  @Input() selectedNumber1: number | null = null;
+  @Input() selectedNumber2: number | null = null;
+  @Input() operation: string | null = null;
+  @Input() result: number | null = null;
 }
